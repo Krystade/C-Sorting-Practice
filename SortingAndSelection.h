9fd043eef9aa2db95213a42e727dfc0f236b5f68
Krys <21113012+Krystade@users.noreply.github.com>
2019-11-25 22:11:27 -0800

    Create SortingAndSelection.h

---
+++ SortingAndSelection.h
@@ -0,0 +1,19 @@
+#ifndef SORTINGANDSELECTION_H_
+#define SORTINGANDSELECTION_H_
+
+#include <iostream>
+#include <ctime>
+#include <math.h>
+using namespace std;
+
+void printArr(int arr[], int size);
+void printArr(float arr[], int size);
+void bubbleSort(int arr[]);
+//void merge(int arr[], int l, int m, int r);
+//void mergeSort(int arr[100]);
+//void partition(arr[], low, high);
+//void quickSort(int arr[], int low, int high);
+void insertionSort(int arr[]);
+
+
+#endif /* SORTINGANDSELECTION_H_ */
